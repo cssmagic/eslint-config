c1e639b07cf1216cfb7b9cec81bef77679e97c17
cssmagic <cssmagic.cn@gmail.com>
2022-05-18 14:40:41 +0800

    refactor: use spread syntax to simplify code, drop support for Node.js 8-

--- preset/essential.js
+++ preset/essential.js
@@ -1,20 +1,18 @@
 const basic = require('../src/config--basic')
 
-let rules = Object.assign(
-	{},
-	require('../src/rule--possible-errors'),
-	require('../src/rule--best-practices'),
-	require('../src/rule--strict-mode'),
-	require('../src/rule--variables'),
-	require('../src/rule--stylistic-issues--critical'),
-	require('../src/rule--ecmascript-6'),
-)
+let rules = {
+	...require('../src/rule--possible-errors'),
+	...require('../src/rule--best-practices'),
+	...require('../src/rule--strict-mode'),
+	...require('../src/rule--variables'),
+	...require('../src/rule--stylistic-issues--critical'),
+	...require('../src/rule--ecmascript-6'),
+}
 
 
-const config = Object.assign(
-	{},
-	basic,
-	{ rules },
-)
+const config = {
+	...basic,
+	rules,
+}
 
 module.exports = config
--- vue.js
+++ vue.js
@@ -1,22 +1,22 @@
 const basic = require('./src/vue/config--basic')
 
-let rules = Object.assign(
-	{},
-	require('./src/vue/rule--_patch'),
-	require('./src/vue/rule--base-rules'),
-	require('./src/vue/rule--priority-a'),
-	require('./src/vue/rule--priority-b'),
-	require('./src/vue/rule--priority-c'),
-	require('./src/vue/rule--uncategorized'),
-	require('./src/vue/rule--_recommended'),
-	require('./src/vue/rule--_stylistic'),
-)
+let rules = {
+	...require('./src/vue/rule--_patch'),
+	...require('./src/vue/rule--base-rules'),
+	...require('./src/vue/rule--priority-a'),
+	...require('./src/vue/rule--priority-b'),
+	...require('./src/vue/rule--priority-c'),
+	...require('./src/vue/rule--uncategorized'),
+
+	...require('./src/vue/rule--_recommended'),
+
+	...require('./src/vue/rule--_stylistic'),
+}
 
 
-const config = Object.assign(
-	{},
-	basic,
-	{ rules },
-)
+const config = {
+	...basic,
+	rules,
+}
 
 module.exports = config
